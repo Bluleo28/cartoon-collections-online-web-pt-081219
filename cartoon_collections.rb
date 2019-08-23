178b39a4f8c5a96f2d2c24e9909b9cab8b1e4755
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-23 03:51:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(veggies)
   end
 end
 
-def long_planeteer_calls(long_planeteer_calls)
+def long_planteer_calls(long_planeteer_calls)
       answer = false 
       long_planeteer_calls.each do |call|
       if call.length > 4
