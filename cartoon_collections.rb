cf08811439db64e44480d263bb04dfef98c7888b
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-23 03:43:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,17 +4,22 @@ def roll_call_dwarves(dwarfs)
   roll_call_dwarves(dwarves)
   end
 end
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)
   veggies.collect do |call|
     call.capatalize + "!"
   end
 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_planeteer_calls)
+  answer = false 
+  long_planeteer_calls.each do |call|
+    if call.length > 4
+      answer = true 
+    end 
+  end
+answer
 end
-
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
