cca7d4cdb0a4fcc55fd0a0a10e93f9c9d2597735
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-23 03:58:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarfs)
 end
 def summon_captain_planet(veggies)
     veggies.collect do |call|
-    call.capatalize + "apple"String
+    call.capatalize + "apple"
   end
 end
 
