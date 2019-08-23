cd7cc691682a6b988c8612c67e2f3a8ae9eb9b98
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-23 04:45:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(array)
         if array.any? {|i|
         i.length > 4}
         return true 
-        else 
+      else 
         return false 
         i = i + 1 
   end 
